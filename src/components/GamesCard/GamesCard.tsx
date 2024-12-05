6b0a7c83b0ff83dce3ab6ab2cfb0e62c9c29bd66
Vitor andes <vitor.andes.santos04@gmail.com>
2024-12-05 17:55:20 -0400

    :sparkles: feat: -- add Owned games -- new style test

--- src/components/GamesCard/GamesCard.tsx
+++ src/components/GamesCard/GamesCard.tsx
@@ -1,46 +1,57 @@
+import { useEffect, useState } from "react";
 import "./GamesCard.less";
 interface Games {
-  game_img: string;
-  game_title: string;
+  steam_appid: number;
+  name: string;
+  header_image: string;
+  genres: { id: string; description: string }[];
 }
 
-const games: Games[] = [
-  {
-    game_img: "",
-    game_title: "Minecraft",
-  },
-  {
-    game_img: "",
-    game_title: "LOL",
-  },
-  {
-    game_img: "",
-    game_title: "Valorant",
-  },
-  {
-    game_img: "",
-    game_title: "Hollow knight",
-  },
-  {
-    game_img: "",
-    game_title: "Celeste",
-  },
-  {
-    game_img: "",
-    game_title: "Little nightmares",
-  },
-];
+interface ApiResponseGames {
+  [steam_appid: number]: {
+    success: boolean;
+    data: Games;
+  };
+}
 
 export function GamesCard() {
+  const [games, setGames] = useState<Games[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const gamesResponse = await fetch("http://localhost:3333/games");
+
+        if (!gamesResponse.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const gamesDetails: ApiResponseGames = await gamesResponse.json();
+        const gamesList = Object.values(gamesDetails).map((item) => item.data);
+
+        setGames(gamesList);
+      } catch (error) {
+        console.error(error instanceof Error ? error.message : "Unknown error");
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div id="games_container">
-      {games.map(({ game_img, game_title }) => {
+      {games.map(({ steam_appid, header_image, name, genres }) => {
         return (
-          <div className="game_card" key={game_title}>
-            <img className="game__img" src={game_img} alt="game__img" />
+          <div className="game_card" key={steam_appid}>
+            <img className="game__img" src={header_image} alt="game__img" />
             <div className="card-label__container">
               <div className="card-label__content">
-                <p>{game_title}</p>
+                <p className="card_label__name">{name}</p>
+              </div>
+              <div className="card-label__genres">
+                {genres.map((genre) => (
+                  <span key={genre.id}>{genre.description}, </span>
+                ))}
               </div>
             </div>
           </div>
