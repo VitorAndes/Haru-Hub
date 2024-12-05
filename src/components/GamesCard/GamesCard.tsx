a7cce1746c7ee513dbcad242cc0f7b28d1ac0362
Vitor andes <vitor.andes.santos04@gmail.com>
2024-12-04 22:25:17 -0400

    :sparkles: feat: -- add userSummaries -- add friendIdList

--- src/components/GamesCard/GamesCard.tsx
+++ src/components/GamesCard/GamesCard.tsx
@@ -6,32 +6,27 @@ interface Games {
 
 const games: Games[] = [
   {
-    game_img:
-      "https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/Homepage_Discover-our-games_MC-Vanilla-KeyArt_864x864.jpg",
+    game_img: "",
     game_title: "Minecraft",
   },
   {
-    game_img:
-      "https://assetsio.gnwcdn.com/co49wj.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp",
+    game_img: "",
     game_title: "LOL",
   },
   {
-    game_img:
-      "https://www.adrenaline.com.br/wp-content/uploads/2023/05/valorant-cover.jpg",
+    game_img: "",
     game_title: "Valorant",
   },
   {
-    game_img:
-      "https://cdn2.steamgriddb.com/grid/d18c832e8c956b4ef8b92862e6bf470d.png",
+    game_img: "",
     game_title: "Hollow knight",
   },
   {
-    game_img: "https://sm.ign.com/ign_br/cover/c/celeste/celeste_y4wf.jpg",
+    game_img: "",
     game_title: "Celeste",
   },
   {
-    game_img:
-      "https://upload.wikimedia.org/wikipedia/pt/1/1b/Little_Nightmares_capa_do_jogo.jpg",
+    game_img: "",
     game_title: "Little nightmares",
   },
 ];
--- src/components/Profile/ProfileCard.tsx
+++ src/components/Profile/ProfileCard.tsx
@@ -1,57 +1,151 @@
+import { useEffect, useState } from "react";
 import "./ProfileCard.less";
 
+type player = {
+  steamid: string;
+  personaname: string;
+  profileurl: string;
+  avatarfull: string;
+  personastate: number;
+  lastlogoff: number;
+  timecreated: number;
+  loccountrycode: string;
+};
+
+type friends = {
+  steamid: string;
+  relationship: string;
+  friend_since: number;
+};
+
+interface ApiResponse {
+  response: {
+    players: player[];
+  };
+}
+interface ApiResponseFriends {
+  friendslist: {
+    friends: friends[];
+  };
+}
+
 export function ProfileCard() {
+  const [player, setPlayer] = useState<player>();
+  const [friends, setFriends] = useState<friends[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [userResponse, friendResponse] = await Promise.all([
+          fetch("http://localhost:3333/user"),
+          fetch("http://localhost:3333/friends"),
+        ]);
+
+        if (!userResponse.ok || !friendResponse.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const userData: ApiResponse = await userResponse.json();
+        const friendData: ApiResponseFriends = await friendResponse.json();
+
+        setPlayer(userData.response.players[0]);
+        setFriends(friendData.friendslist.friends);
+      } catch (error) {
+        console.error(error instanceof Error ? error.message : "Unknown error");
+      }
+    };
+    fetchData();
+  }, []);
+
+  const getPlayerStatus = (personastate: number) => {
+    switch (personastate) {
+      case 0:
+        return "Offline";
+      case 1:
+        return "Online";
+      case 2:
+        return "Busy";
+      case 3:
+        return "Away";
+      case 4:
+        return "Snooze";
+      case 5:
+        return "looking to trade";
+      default:
+        return "Unknown";
+    }
+  };
+
   return (
     <div id="Container_profile">
       <div className="profile">
-        <div className="profile_avatar">
-          <img
-            src="https://pt.quizur.com/_image?href=https://img.quizur.com/f/img637e1ff08f6e90.51513695.png?lastEdited=1669210099&w=600&h=600&f=webp"
-            alt="avatar"
-          />
-          <h1>Haru Moon</h1>
-        </div>
-        <div className="profile_status">
-          <span>Playing...</span>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-            expedita ut quibusdam accusamus
-          </p>
-        </div>
+        {player ? (
+          <div className="profile_avatar">
+            <img
+              src={player.avatarfull}
+              alt={`${player.personaname}'s avatar`}
+            />
+            <a
+              href={player.profileurl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View Profile
+            </a>
+          </div>
+        ) : (
+          <p>No player data available</p>
+        )}
+
+        {player ? (
+          <div className="profile_status">
+            <h2>{player.personaname}</h2>
+            <span>{getPlayerStatus(player.personastate)}</span>
+            <p>Gamer proffisa</p>
+          </div>
+        ) : (
+          <p>No player data available</p>
+        )}
       </div>
-      <div className="container_achievements">
+
+      {/* <div className="container_achievements">
         <h2>Achievements</h2>
         <div className="achievements">
           <span>achievements</span>
           <span>achievements</span>
           <span>achievements</span>
         </div>
-      </div>
-      <div className="container_insignia">
+      </div> */}
+
+      {/* <div className="container_insignia">
         <h2>insignia</h2>
         <div className="insignia">
           <span>insignia</span>
           <span>insignia</span>
           <span>insignia</span>
         </div>
-      </div>
-      <div className="container_infos">
+      </div> */}
+
+      {/* <div className="container_infos">
         <span>info</span>
         <span>info</span>
         <span>info</span>
-      </div>
+      </div> */}
 
       <div className="container_friends">
         <h2>Friends</h2>
-        <div className="friend">
-          <span>friend_avatar</span>
-          <span>friend_name</span>
-        </div>
-
-        <div className="friend">
-          <span>friend_avatar</span>
-          <span>friend_name</span>
-        </div>
+        {friends ? (
+          friends.map((friend) => {
+            return (
+              <div className="friend" key={friend.steamid}>
+                <span>{friend.friend_since}</span>
+                <span>{friend.relationship}</span>
+              </div>
+            );
+          })
+        ) : (
+          <p>No friend data</p>
+        )}
       </div>
     </div>
   );
